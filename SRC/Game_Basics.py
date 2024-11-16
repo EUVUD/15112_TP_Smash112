272f06dc110a1a8e50b69d6edb72d9dc0bc6278c
EUVUD <junning.david.hu@gmail.com>
2024-11-16 14:48:33 -0500

    Shooting ability added, no collision yet

--- SRC/Game_Basics.py
+++ SRC/Game_Basics.py
@@ -3,20 +3,36 @@ import Game_Char
 
 def onAppStart(app):
     app.ground = 300
-    app.player1 = Game_Char.Char('Orange', app.width/4, app.height/4, 20, 'orange')
+    app.projection = []
+    app.player1 = Game_Char.char('Orange', app.width/4, app.height/4, 20, 'orange', None, app.projection)
 
 
 def redrawAll(app):
+    # draw the character
     drawRect(0, app.ground, app.width, app.height, fill = 'black')
-    drawRect(app.player1.x, app.player1.y, app.player1.size
-             , app.player1.size, fill = app.player1.color)
+    if app.player1.direction == 'left':
+        drawRect(app.player1.x, app.player1.y, app.player1.size,
+                 app.player1.size, fill = app.player1.color)
+        drawLine(app.player1.x+app.player1.size, app.player1.y+app.player1.size/2,
+                 app.player1.x, app.player1.y+app.player1.size/2, arrowEnd = True)
+    else:
+        drawRect(app.player1.x, app.player1.y, app.player1.size
+                , app.player1.size, fill = app.player1.color)
+        drawLine(app.player1.x, app.player1.y+app.player1.size/2,
+                 app.player1.x+app.player1.size, app.player1.y+app.player1.size/2, arrowEnd = True)
         
+
+    # draw the Bullet
+    for bullet in app.projection:
+        drawCircle(bullet.x, bullet.y, bullet.size, fill = 'black')
 
 def onKeyPress(app, key):
     if key == 'up':
         if app.player1.jump == False:
             app.player1.jump = True
             jumpPlay(app, app.player1)
+    if key == 'k':
+        app.player1.shoot()
 
 def jumpPlay(app, player):
     app.player1.dy = -35
@@ -25,8 +41,10 @@ def jumpPlay(app, player):
 def onKeyHold(app, keys):
     if 'left' in keys:
         app.player1.x -= 5
+        app.player1.direction = 'left'
     elif 'right' in keys:
         app.player1.x += 5
+        app.player1.direction = 'right'
     if app.player1.x + app.player1.size > app.width: #Bounded Motion
         app.player1.x = app.width-app.player1.size
     elif app.player1.x < 0:
@@ -34,6 +52,7 @@ def onKeyHold(app, keys):
 
 def onStep(app):
     takeStep(app)
+    bulletFly(app)
 
 def takeStep(app):
     if app.player1.jump == True:
@@ -43,6 +62,24 @@ def takeStep(app):
         app.player1.y += 10
     if app.player1.y >= app.ground - app.player1.size:
         app.player1.jump = False
+
+def bulletFly(app):
+    # for i in range(len(app.projection)):
+    #     app.projection[i].x += app.projection[i].velocity
+    #     if app.projection[i].x - app.projection[i].size < 0:
+    #         app.projection.pop(i)
+    #     elif app.projection[i].x + app.projection[i].size > app.width:
+    #         app.projection.pop(i)
+
+    index = 0
+    while index < len(app.projection):
+        app.projection[index].x += app.projection[index].velocity
+        if app.projection[index].x - app.projection[index].size < 0:
+            app.projection.pop(index)
+        elif app.projection[index].x + app.projection[index].size > app.width:
+            app.projection.pop(index)
+        index += 1
+
 
 
 runApp()
--- SRC/Game_Char.py
+++ SRC/Game_Char.py
@@ -1,5 +1,12 @@
-class Char:
-    def __init__(self, name, x, y, size, color):
+class bullet:
+    def __init__(self, name, x, y, size, velocity):
+        self.x = x
+        self.y = y
+        self.size = size
+        self.velocity = velocity
+
+class char:
+    def __init__(self, name, x, y, size, color, direction, bulletList):
         self.name = name
         self.x = x
         self.y = y
@@ -7,6 +14,25 @@ class Char:
         self.jump = False
         self.color = color
         self.dy = 0
+        self.direction = direction
+        self.bulletList = bulletList
 
     def __repr__(self):
         return f'{self.name} with size {self.size} is at ({self.x},{self.y})'
+    
+    def shoot(self):
+        yPos = self.y + self.size/2
+        velocity = None
+        if self.direction == 'left':
+            xPos = self.x
+            velocity = -10
+        else:
+            xPos = self.x + self.size
+            velocity = 10
+        newBullet = bullet('bullet', xPos, yPos, 3, velocity)
+        self.bulletList.append(newBullet)
+
+
+
+
+
