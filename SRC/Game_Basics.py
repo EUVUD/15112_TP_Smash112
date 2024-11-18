08ce465492464434eddc143dce2b932919dfd2ef
EUVUD <junning.david.hu@gmail.com>
2024-11-17 19:09:49 -0500

    11.17 Daily update

--- SRC/Game_Basics.py
+++ SRC/Game_Basics.py
@@ -8,6 +8,9 @@ def onAppStart(app):
 
 
 def redrawAll(app):
+    # Instruction:
+    drawLabel('Use left, up, right, down to move the block', app.width/2, 20, size = 14)
+    drawLabel('Use k to shoot the bullet', app.width/2, 35, size = 14)
     # draw the character
     drawRect(0, app.ground, app.width, app.height, fill = 'black')
     if app.player1.direction == 'left':
@@ -27,18 +30,22 @@ def redrawAll(app):
         drawCircle(bullet.x, bullet.y, bullet.size, fill = 'black')
 
 def onKeyPress(app, key):
+    # Player1 Action
+    #Jump
     if key == 'up':
         if app.player1.jump == False:
-            app.player1.jump = True
             jumpPlay(app, app.player1)
+    #Shoot the bullet
     if key == 'k':
         app.player1.shoot()
 
 def jumpPlay(app, player):
+    app.player1.jump = True
     app.player1.dy = -35
     
 
 def onKeyHold(app, keys):
+    # Still Player1 Movement
     if 'left' in keys:
         app.player1.x -= 5
         app.player1.direction = 'left'
@@ -51,26 +58,25 @@ def onKeyHold(app, keys):
         app.player1.x = 0
 
 def onStep(app):
-    takeStep(app)
+    gravSimul(app)
     bulletFly(app)
 
-def takeStep(app):
+#Gravity Simulation
+
+def gravSimul(app):
     if app.player1.jump == True:
         app.player1.dy += 2
         app.player1.y += app.player1.dy
+    # In the air, the player keeps falling
     if app.player1.y < app.ground - app.player1.size:
         app.player1.y += 10
+    else: # Stay on ground
+        app.player1.y = app.ground - app.player1.size
     if app.player1.y >= app.ground - app.player1.size:
         app.player1.jump = False
 
+#Bullet fly function
 def bulletFly(app):
-    # for i in range(len(app.projection)):
-    #     app.projection[i].x += app.projection[i].velocity
-    #     if app.projection[i].x - app.projection[i].size < 0:
-    #         app.projection.pop(i)
-    #     elif app.projection[i].x + app.projection[i].size > app.width:
-    #         app.projection.pop(i)
-
     index = 0
     while index < len(app.projection):
         app.projection[index].x += app.projection[index].velocity
